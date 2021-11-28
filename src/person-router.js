3b00cf4bd26824d1cca0230ea9b5166df4f332ab
Konstantin <name.gku@gmail.com>
2021-11-28 18:07:03 +0300

    Implemented put & delete requests

--- src/person-router.js
+++ src/person-router.js
@@ -1,7 +1,7 @@
 const { v4: uuidv4, validate: uuidValidate } = require('uuid');
 const Router = require('./framework/Router');
 
-const persons = [];
+let persons = [];
 
 
 const router = new Router();
@@ -43,6 +43,58 @@ router.post('/person', (req, res) => {
   const newPerson = { ...person, id: uuidv4() };
   persons.push(newPerson);
   return res.send(201, newPerson);
+});
+
+router.put('/person/:id', (req, res) => {
+  const personId = req.personId;
+  const newPersonData = req.body;
+  let updatedPerson = {};
+
+  if (!uuidValidate(personId)) {
+    return res.send(400, 'invalid personId');
+  };
+
+  persons = persons.map(person => {
+    if (person.id === personId) {
+      updatedPerson = {
+        ...person,
+        ...newPersonData
+      };
+      return updatedPerson
+    }
+  });
+
+  if (Object.keys(updatedPerson).length !== 0) {
+    return res.send(200, updatedPerson);
+  } else {
+    return res.send(404, "person didn\'t find");
+  };
+
+
+});
+
+router.delete('/person/:id', (req, res) => {
+  const personId = req.personId;
+  let isDeleted = false;
+
+  if (!uuidValidate(personId)) {
+    return res.send(400, 'invalid personId');
+  };
+
+  persons = persons.filter(person => {
+    if (person.id !== personId) {
+      return true;
+    } else {
+      isDeleted = true;
+      return false;
+    };
+  });
+
+  if (isDeleted) {
+    return res.send(204, "person was delete");
+  } else {
+    return res.send(404, "person didn\'t find");
+  };
 });
 
 module.exports = router;
